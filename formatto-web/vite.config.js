bf095a4fd1744ed1de03b2c38ef9a4bfeae257f9
casasornelasdaniel120 <casasornelasdaniel@gmail.com>
2026-03-27 14:12:51 -0600

    Remove viteSingleFile plugin and update config

--- formatto-web/vite.config.js
+++ formatto-web/vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react(), viteSingleFile({ removeViteModuleLoader: true })],
-  base: './',
-  build: {
-    assetsInlineLimit: 100000000,
-    cssCodeSplit: false,
-    modulePreload: false,
-    minify: 'terser',
-  },
+  plugins: [react()],
 })
